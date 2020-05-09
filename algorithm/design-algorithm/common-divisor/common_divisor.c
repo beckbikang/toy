825883c77e07101af4e5781ca785b31f305a9235
毕康 <bikang1@staff.weibo.com>
2020-05-09 22:15:01 +0800

    go

--- algorithm/design-algorithm/common-divisor/common_divisor.c
+++ algorithm/design-algorithm/common-divisor/common_divisor.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 
-
+int max(int x, int y ){
+    if(x > y) {
+        return  x;
+    }
+}
 
 int main(int argc, char const *argv[])
 {
--- rust-explorer/grammar/rcollection/src/main.rs
+++ rust-explorer/grammar/rcollection/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     tcolloct();
 }
@@ -38,8 +40,50 @@ fn tcolloct(){
     let s2 = "hello rust";
     println!("s2={}", s2.to_string());
     let mut s3 = String::from("abc");
-    s3.push("a");
+    s3.push_str("a");
+    println!("{}", &s2[0..4]);
 
+    let s11 = String::from("tic");
+    let s12 = String::from("tac");
+    let s13 = String::from("toe");
+
+    let s14 = s11 + "-" + &s12 + "-" + &s13;
+
+    let s15 = format!("{}-{}-{}", s14, s12, s13);
+    println!("s15-{}",s15);
+    for c in s15.bytes(){
+        //println!("{}",c);
+    }
+
+    //hash map
+    let mut scores = HashMap::new();
+    scores.insert(String::from("abcc"), 10);
+    scores.insert(String::from("def"), 10);
+
+    let teams  = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+    
+    let scores2: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+
+    println!("scores2:{:?}", scores2);
+
+    let teamname = String::from("Blue");
+    let ts = scores2.get(&teamname);
+    println!("{:?}", ts);
+    
+    let mut scores22 = HashMap::new();
+
+    scores22.insert(String::from("Blue"), 10);
+    scores22.insert(String::from("Yellow"), 50);
+    let score_int = 222;
+    let blue1 = String::from("Blue");
+    scores22.insert(blue1, score_int);
+
+    scores22.entry(String::from("Blue")).or_insert(5555);
+
+    for (key, value) in &scores22{
+        println!("{}:{}", key, value);
+    }
 
 
 }
