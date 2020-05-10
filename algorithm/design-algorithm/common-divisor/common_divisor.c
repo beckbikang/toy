ba48a7ff9cd48ca94809f04fc9771be8a834d97a
毕康 <bikang1@staff.weibo.com>
2020-05-10 18:55:33 +0800

    高斯

--- algorithm/design-algorithm/common-divisor/common_divisor.c
+++ algorithm/design-algorithm/common-divisor/common_divisor.c
@@ -1,13 +1,44 @@
 #include<stdio.h>
 
+
+int gcd(int m, int n){
+
+    int r;
+
+    while(n >0){
+        r = m %n;
+        m = n;
+        n = r;
+    }
+    return m;
+}
+
 int max(int x, int y ){
     if(x > y) {
         return  x;
     }
+    return y;
 }
+
+float getPi(long n){
+
+    long i = 1;
+    long cent = 0;
+    float ret = 0;
+
+    while(i < n){
+        cent = 2*i;
+        ret += 1.0/(cent-1) - 1.0/(cent+1);
+        i = i+2;
+    }
+
+    return  ret * 4;
+}
+
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    printf("%d,%d => %d\n",60,24, 12);
+    printf("%2.10f", getPi(1000000000));
     return 0;
 }
