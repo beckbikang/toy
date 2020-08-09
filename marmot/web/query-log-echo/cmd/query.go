c0296526d0ff4faebb15e98be9db78e16ec78517
毕康 <bikang1@staff.weibo.com>
2020-08-09 19:34:53 +0800

    接入cobra

--- marmot/web/query-log-echo/cmd/query.go
+++ marmot/web/query-log-echo/cmd/query.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"log"
 	"strings"
 	"github.com/spf13/cobra"
 	"toy/marmot/web/query-log-echo/launch"
@@ -21,11 +22,18 @@ var queryCmd = &cobra.Command{
 	Short: "web日志查询系统",
 	Long:  desc,
 	Run: func(cmd *cobra.Command, args []string) {
+
+		if confRoot == ""{
+
+			log.Fatalf("暂不支持该转换模式，请执行 help query 查看帮助文档")
+			return
+		}
+
 		launch.InitLaunchWithParam(confRoot, env)
 	},
 }
 
 func init()  {
-	queryCmd.Flags().StringVarP(&confRoot, "confRoot", "c", "./conf", "root path of configuration")
+	queryCmd.Flags().StringVarP(&confRoot, "confRoot", "c", "", "root path of configuration")
 	queryCmd.Flags().StringVarP(&env, "env", "e", "dev", "prod or dev or test")
 }
